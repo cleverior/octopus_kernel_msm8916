606adfed16a95f9969f6c09f0d07310109b643b1
Snuzzo <jrracinfan@gmail.com>
2015-07-22 19:03:41 -0700

    cpufreq: Add dancedance governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -473,6 +473,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CAFACTIVE)
 extern struct cpufreq_governor cpufreq_gov_cafactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cafactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
+extern struct cpufreq_governor cpufreq_gov_dancedance;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_DANCEDANCE)
 #endif
 
 /*********************************************************************
